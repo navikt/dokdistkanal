1c19300b6f80a8590aaab0c3d50ed3385de33532
tsigab <tsigab.angosom@gmail.com>
2023-06-02 14:23:15 +0200

    MMA-6178 endret webclient til resttemplate etter itest feil.

--- app/src/main/java/no/nav/dokdistkanal/consumer/altinn/maskinporten/MaskinportenConsumer.java
+++ app/src/main/java/no/nav/dokdistkanal/consumer/altinn/maskinporten/MaskinportenConsumer.java
@@ -4,31 +4,39 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jwt.JWTClaimsSet;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import no.nav.dokdistkanal.common.NavHeadersFilter;
 import no.nav.dokdistkanal.config.properties.MaskinportenProperties;
 import no.nav.dokdistkanal.exceptions.functional.MaskinportenFunctionalException;
 import no.nav.dokdistkanal.exceptions.technical.MaskinportenTechnicalException;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.UUID;
 
 import static java.util.Date.from;
+import static no.nav.dokdistkanal.common.FunctionalUtils.getOrCreateCallId;
 import static no.nav.dokdistkanal.config.cache.LocalCacheConfig.MASKINPORTEN_CACHE;
 import static no.nav.dokdistkanal.constants.DomainConstants.DEFAULT_ZONE_ID;
 import static no.nav.dokdistkanal.constants.DomainConstants.NAV_ORGNUMMER;
 import static no.nav.dokdistkanal.constants.MDCConstants.CALL_ID;
+import static no.nav.dokdistkanal.constants.MDCConstants.NAV_CALL_ID;
 import static no.nav.dokdistkanal.consumer.altinn.maskinporten.Authority.ISO_6523_ACTORID_UPIS;
 import static no.nav.dokdistkanal.consumer.altinn.maskinporten.MaskinportenUtils.asIso6523;
 import static no.nav.dokdistkanal.consumer.altinn.maskinporten.MaskinportenUtils.createSignedJWT;
-import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
-import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
+import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED;
 
 @Slf4j
 @Component
@@ -37,17 +45,16 @@ public class MaskinportenConsumer {
 	public static final String FUNKSJONELL_FEIL_ERROR_MESSAGE = "Klarte ikke hente AccessToken fra maskinporten. Funksjonell feil: ";
 	public static final String TEKNISK_FEIL_ERROR_MESSAGE = "Klarte ikke hente AccessToken fra maskinporten. Teknisk feil: ";
 
-	private final WebClient webClient;
+	private final RestTemplate restTemplate;
 	private final MaskinportenProperties maskinportenProperties;
 
 	@Autowired
-	public MaskinportenConsumer(WebClient webClient,
+	public MaskinportenConsumer(RestTemplateBuilder restTemplateBuilder,
 								MaskinportenProperties maskinportenProperties) {
 		this.maskinportenProperties = maskinportenProperties;
-		this.webClient = webClient.mutate()
-				.baseUrl(maskinportenProperties.getTokenEndpoint())
-				.defaultHeader(CONTENT_TYPE, APPLICATION_FORM_URLENCODED_VALUE)
-				.filter(new NavHeadersFilter(CALL_ID))
+		this.restTemplate = restTemplateBuilder
+				.setReadTimeout(Duration.ofSeconds(20))
+				.setConnectTimeout(Duration.ofSeconds(5))
 				.build();
 	}
 
@@ -58,25 +65,20 @@ public class MaskinportenConsumer {
 		attrMap.add("grant_type", "urn:ietf:params:oauth:grant-type:jwt-bearer");
 		attrMap.add("assertion", generateJWT());
 
-		OidcTokenResponse tokenResponse = webClient.post()
-				.uri(maskinportenProperties.getTokenEndpoint())
-				.bodyValue(attrMap)
-				.retrieve()
-				.bodyToMono(OidcTokenResponse.class)
-				.doOnError(err -> {
-					if (err instanceof WebClientResponseException respose &&
-							((WebClientResponseException) err).getStatusCode().is4xxClientError()) {
-						final String errorMessage = FUNKSJONELL_FEIL_ERROR_MESSAGE + respose.getResponseBodyAsString();
-						log.warn(errorMessage, errorMessage);
-						throw new MaskinportenFunctionalException(errorMessage, err);
-					}
-					final String errorMessage = TEKNISK_FEIL_ERROR_MESSAGE + err.getMessage();
-					log.error(errorMessage, err);
-					throw new MaskinportenTechnicalException(errorMessage, err);
-				})
-				.block();
+		HttpEntity httpEntity = new HttpEntity<>(attrMap, headers());
 
-		return tokenResponse.getAccessToken();
+		try {
+			ResponseEntity<OidcTokenResponse> tokenResponse = restTemplate.exchange(maskinportenProperties.getTokenEndpoint(), HttpMethod.POST, httpEntity, OidcTokenResponse.class);
+			return tokenResponse.getBody().getAccessToken();
+		} catch (HttpClientErrorException err) {
+			final String errorMessage = FUNKSJONELL_FEIL_ERROR_MESSAGE + err.getResponseBodyAsString();
+			log.warn(errorMessage, errorMessage);
+			throw new MaskinportenFunctionalException(errorMessage, err);
+		} catch (HttpServerErrorException err) {
+			final String errorMessage = TEKNISK_FEIL_ERROR_MESSAGE + err.getMessage();
+			log.error(errorMessage, err);
+			throw new MaskinportenTechnicalException(errorMessage, err);
+		}
 	}
 
 	@SneakyThrows
@@ -103,5 +105,12 @@ public class MaskinportenConsumer {
 		ArrayList<String> scopeList = new ArrayList<>();
 		scopeList.add(maskinportenProperties.getScopes());
 		return scopeList.stream().reduce((a, b) -> a + " " + b).orElse("");
+	}
+
+	private HttpHeaders headers() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(APPLICATION_FORM_URLENCODED);
+		headers.set(NAV_CALL_ID, getOrCreateCallId(MDC.get(CALL_ID)));
+		return headers;
 	}
 }
--- app/src/main/java/no/nav/dokdistkanal/consumer/altinn/serviceowner/AltinnServiceOwnerConsumer.java
+++ app/src/main/java/no/nav/dokdistkanal/consumer/altinn/serviceowner/AltinnServiceOwnerConsumer.java
@@ -1,59 +1,78 @@
 package no.nav.dokdistkanal.consumer.altinn.serviceowner;
 
-import no.nav.dokdistkanal.common.NavHeadersFilter;
-import no.nav.dokdistkanal.config.WebClientAuthentication;
 import no.nav.dokdistkanal.config.properties.DokdistkanalProperties;
 import no.nav.dokdistkanal.consumer.altinn.maskinporten.MaskinportenConsumer;
 import no.nav.dokdistkanal.exceptions.functional.AltinnServiceOwnerFunctionalException;
 import no.nav.dokdistkanal.exceptions.technical.AltinnServiceOwnerTechnicalException;
 import no.nav.dokdistkanal.exceptions.technical.DokDistKanalTechnicalException;
+import org.slf4j.MDC;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.time.Duration;
+
+import static no.nav.dokdistkanal.common.FunctionalUtils.getOrCreateCallId;
 import static no.nav.dokdistkanal.constants.MDCConstants.CALL_ID;
+import static no.nav.dokdistkanal.constants.MDCConstants.NAV_CALL_ID;
 import static org.springframework.hateoas.MediaTypes.HAL_JSON_VALUE;
 import static org.springframework.http.HttpHeaders.ACCEPT;
+import static org.springframework.http.HttpMethod.GET;
 
 @Component
 public class AltinnServiceOwnerConsumer {
 
 	private static final String SERVICEOWNER_PATH = "/serviceowner/notifications/validaterecipient";
 
-	private final WebClient webClient;
+	private final MaskinportenConsumer maskinportenConsumer;
 	private final DokdistkanalProperties dokdistkanalProperties;
+	private final RestTemplate restTemplate;
 
 	public AltinnServiceOwnerConsumer(DokdistkanalProperties dokdistkanalProperties,
-									  WebClient webClient,
+									  RestTemplateBuilder restTemplateBuilder,
 									  MaskinportenConsumer maskinportenConsumer) {
+		this.maskinportenConsumer = maskinportenConsumer;
 		this.dokdistkanalProperties = dokdistkanalProperties;
-		this.webClient = webClient.mutate()
-				.baseUrl(dokdistkanalProperties.getAltinn().getUrl())
-				.defaultHeaders(httpHeaders -> httpHeaders.set(ACCEPT, HAL_JSON_VALUE))
-				.filter(new NavHeadersFilter(CALL_ID))
-				.filter(new WebClientAuthentication(maskinportenConsumer.getMaskinportenToken()))
+		this.restTemplate = restTemplateBuilder
+				.setReadTimeout(Duration.ofSeconds(20))
+				.setConnectTimeout(Duration.ofSeconds(5))
 				.build();
 	}
 
 	@Retryable(retryFor = DokDistKanalTechnicalException.class, backoff = @Backoff(delay = 200))
 	public ValidateRecipientResponse isServiceOwnerValidReciepient(String orgNummer) {
-		return webClient.get()
-				.uri(uriBuilder -> uriBuilder.path(SERVICEOWNER_PATH)
-						.queryParam("organizationNumber", orgNummer)
-						.queryParam("serviceCode", dokdistkanalProperties.getAltinn().getServiceCode())
-						.queryParam("serviceEditionCode", dokdistkanalProperties.getAltinn().getServiceEditionCode())
-						.build())
-				.header("ApiKey", dokdistkanalProperties.getAltinn().getApiKey())
-				.retrieve()
-				.bodyToMono(ValidateRecipientResponse.class)
-				.doOnError(err -> {
-					if (err instanceof WebClientResponseException response && ((WebClientResponseException) err).getStatusCode().is4xxClientError()) {
-						throw new AltinnServiceOwnerFunctionalException(err.getMessage(), err);
-					}
-					throw new AltinnServiceOwnerTechnicalException(err.getMessage(), err);
-				})
-				.block();
+		String altinnUrl = UriComponentsBuilder.fromUriString(dokdistkanalProperties.getAltinn().getUrl())
+				.path(SERVICEOWNER_PATH)
+				.queryParam("organizationNumber", orgNummer)
+				.queryParam("serviceCode", dokdistkanalProperties.getAltinn().getServiceCode())
+				.queryParam("serviceEditionCode", dokdistkanalProperties.getAltinn().getServiceEditionCode())
+				.build().toString();
+
+		HttpEntity httpEntity = new HttpEntity<>(headers());
+		try {
+			ResponseEntity<ValidateRecipientResponse> response = restTemplate.exchange(altinnUrl, GET, httpEntity, ValidateRecipientResponse.class);
+			return response.getBody();
+		} catch (HttpClientErrorException err) {
+			throw new AltinnServiceOwnerFunctionalException(err.getMessage(), err);
+		} catch (HttpServerErrorException err) {
+			throw new AltinnServiceOwnerTechnicalException(err.getMessage(), err);
+		}
+	}
+
+	private HttpHeaders headers() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set(ACCEPT, HAL_JSON_VALUE);
+		headers.setBearerAuth(maskinportenConsumer.getMaskinportenToken());
+		headers.set("ApiKey", dokdistkanalProperties.getAltinn().getApiKey());
+		headers.set(NAV_CALL_ID, getOrCreateCallId(MDC.get(CALL_ID)));
+		return headers;
 	}
 }
