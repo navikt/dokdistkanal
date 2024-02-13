5febae7c7b98f6beb5ae840a7f9298d51327560f
Anders Opskar Voldsund <anders.voldsund@nav.no>
2024-02-13 08:43:23 +0100

    MMA-7188 Oppgrader til Java 21
    Fiks test der token support returnerte 500 i staden for 401 sidan token var på feil format.

--- app/src/test/java/no/nav/dokdistkanal/rest/bestemdistribusjonskanal/BestemDistribusjonskanalIT.java
+++ app/src/test/java/no/nav/dokdistkanal/rest/bestemdistribusjonskanal/BestemDistribusjonskanalIT.java
@@ -500,10 +500,11 @@ public class BestemDistribusjonskanalIT extends AbstractIT {
 
 	@Test
 	void skalReturnereUnauthorizedVedUgyldigOIDCToken() {
+		var ugyldigToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQss123";
 
 		var response = webTestClient.post()
 				.uri(BESTEM_DISTRIBUSJONSKANAL_URL)
-				.header(AUTHORIZATION, "Bearer " + "ugyldig-token")
+				.header(AUTHORIZATION, "Bearer " + ugyldigToken)
 				.bodyValue(bestemDistribusjonskanalRequest())
 				.exchange()
 				.expectStatus()
