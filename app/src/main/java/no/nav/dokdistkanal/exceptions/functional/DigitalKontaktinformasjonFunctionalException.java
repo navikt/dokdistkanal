58bb816f185c7be7106232d36252f9db4849eb3e
Anders Opskar Voldsund <anders.voldsund@nav.no>
2024-03-13 11:18:35 +0100

    MMA-7352 Fiks retry i DigitalKontaktinformasjonConsumer og refaktorer litt
    DigitalKontaktinformasjonTechnicalException var av typen DokdistkanalFunctionalException som gjorde at den ikkje fekk retry.

--- app/src/main/java/no/nav/dokdistkanal/exceptions/functional/DigitalKontaktinformasjonFunctionalException.java
+++ app/src/main/java/no/nav/dokdistkanal/exceptions/functional/DigitalKontaktinformasjonFunctionalException.java
@@ -1,9 +1,10 @@
 package no.nav.dokdistkanal.exceptions.functional;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@ResponseStatus(value = BAD_REQUEST)
 public class DigitalKontaktinformasjonFunctionalException extends DokdistkanalFunctionalException {
 	public DigitalKontaktinformasjonFunctionalException(String message, Throwable cause) {
 		super(message, cause);
--- app/src/main/java/no/nav/dokdistkanal/exceptions/technical/DigitalKontaktinformasjonTechnicalException.java
+++ app/src/main/java/no/nav/dokdistkanal/exceptions/technical/DigitalKontaktinformasjonTechnicalException.java
@@ -1,18 +1,13 @@
 package no.nav.dokdistkanal.exceptions.technical;
 
-import no.nav.dokdistkanal.exceptions.functional.DokdistkanalFunctionalException;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import static org.springframework.http.HttpStatus.SERVICE_UNAVAILABLE;
 
 @ResponseStatus(value = SERVICE_UNAVAILABLE)
-public class DigitalKontaktinformasjonTechnicalException extends DokdistkanalFunctionalException {
+public class DigitalKontaktinformasjonTechnicalException extends DokdistkanalTechnicalException {
 
 	public DigitalKontaktinformasjonTechnicalException(String message, Throwable cause) {
 		super(message, cause);
-	}
-
-	public DigitalKontaktinformasjonTechnicalException(String message) {
-		super(message);
 	}
 }
