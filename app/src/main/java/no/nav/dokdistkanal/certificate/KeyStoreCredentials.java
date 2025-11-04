3528203245fdb188d566199b84509282f0c87c9a
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2025-11-04 10:16:46 +0100

    MMA-7732 Mindre tilpasninger
    * Override toString i KeyStoreCredentials record
    * Ryddet ubrukte verdier

--- app/src/main/java/no/nav/dokdistkanal/certificate/KeyStoreCredentials.java
+++ app/src/main/java/no/nav/dokdistkanal/certificate/KeyStoreCredentials.java
@@ -1,4 +1,14 @@
 package no.nav.dokdistkanal.certificate;
 
+import java.util.StringJoiner;
+
 record KeyStoreCredentials(String alias, String password, String type) {
+	@Override
+	public String toString() {
+		return new StringJoiner(", ", KeyStoreCredentials.class.getSimpleName() + "[", "]")
+				.add("alias='" + alias + "'")
+				.add("password='*****'")
+				.add("type='" + type + "'")
+				.toString();
+	}
 }
